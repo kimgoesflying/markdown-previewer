1b4cdbc2b9d6b0da3082e5eea2b5c8ba489ba266
elizarova <yelizarova.ae@gmail.com>
2020-07-20 13:59:14 +0300

    navBar hooks

--- src/components/NavbarComponent.js
+++ src/components/NavbarComponent.js
@@ -1,36 +1,25 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import HelpModalComponent from './HelpModalComponent';
 
-class NavbarComponent extends Component {
-  state = { show: false };
+const NavbarComponent = () => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-  showModal = () => {
-    this.setState({ show: true });
-  };
-
-  hideModal = () => {
-    this.setState({ show: false });
-  };
-
-  render() {
-    return (
-      <Navbar bg="dark" variant="dark" className="justify-content-between">
-        <Navbar.Brand className="ml-2" href="/">
-          Markdown Previewer
-        </Navbar.Brand>
-        <HelpModalComponent
-          show={this.state.show}
-          handleClose={this.hideModal}
-        />
-        <Navbar.Text className="mr-2">
-          <a href="#!" onClick={this.showModal}>
-            <i className="far fa-question-circle fa-2x"></i>
-          </a>
-        </Navbar.Text>
-      </Navbar>
-    );
-  }
-}
+  return (
+    <Navbar bg="dark" variant="dark" className="justify-content-between">
+      <Navbar.Brand className="ml-2" href="/">
+        Markdown Previewer
+      </Navbar.Brand>
+      <HelpModalComponent show={show} handleClose={handleClose} />
+      <Navbar.Text className="mr-2">
+        <a href="#!" onClick={handleShow}>
+          <i className="far fa-question-circle fa-2x"></i>
+        </a>
+      </Navbar.Text>
+    </Navbar>
+  );
+};
 
 export default NavbarComponent;
